c5aee283ab27443deb058511caf3e53f9348db6f
Eduardo Maurício Siqueira Borges Filho <edborgesfh@gmail.com>
2024-11-21 11:17:23 -0300

    'adaptações para diferentes telas'

--- src/app_performance_test.py
+++ src/app_performance_test.py
@@ -12,15 +12,62 @@ from scipy.signal import convolve
 import dash_bootstrap_components as dbc
 import soundfile as sf
 from pathlib import Path
+import uuid
 
 app = dash.Dash(__name__, external_stylesheets=[dbc.themes.DARKLY, 'src/assets/style.css'])
 server = app.server
 
-# Diretório dos arquivos de áudio
+# Configurações
 AUDIO_DIR = Path(__file__).parent / 'basslines'
+FREQ_TICKS = [20, 50, 70, 100, 150, 250, 500, 1000, 1500, 2000, 3000, 5000, 10000, 15000, 20000]
+DEFAULT_COLOR = '#FF5C00'
+IR_COLOR = '#FFDF00'
 
-# Labels para as frequências
-freq_ticks = [20, 50, 70, 100, 150, 250, 500, 1000, 1500, 2000, 3000, 5000, 10000, 15000, 20000]
+# Função para calcular e armazenar em cache os dados do áudio
+audio_cache = {}
+
+def process_audio(filepath_or_audio, sr=None):
+    filepath = None
+    if isinstance(filepath_or_audio, Path):
+        filepath = filepath_or_audio
+        cache_key = str(filepath)
+    elif isinstance(filepath_or_audio, np.ndarray):
+        cache_key = str(uuid.uuid4())  # Gere uma chave única para dados brutos
+        sr = sr # Use sr fornecido
+    else:
+        raise TypeError("Input deve ser um caminho de arquivo ou dados de áudio numpy.")
+
+    if cache_key in audio_cache:
+        return audio_cache[cache_key]
+
+    if filepath:
+        y, sr = librosa.load(filepath)
+    else:
+        y = filepath_or_audio
+
+    n_fft = 2048
+    hop_length = n_fft // 4
+    stft = librosa.stft(y, n_fft=n_fft, hop_length=hop_length)
+    power = np.abs(stft)**2
+    ref_power = np.mean(power)
+    relative_spl_db = 10 * np.log10(power / ref_power)
+    freq = librosa.fft_frequencies(sr=sr, n_fft=n_fft)
+    magnitude_db = librosa.amplitude_to_db(np.abs(stft), ref=np.max)
+    time = librosa.times_like(magnitude_db, sr=sr, hop_length=hop_length)
+    mean_spl_db = np.mean(relative_spl_db, axis=1)
+    duration = librosa.get_duration(y=y, sr=sr)
+
+    audio_data = {
+        'y': y,
+        'sr': sr,
+        'freq': freq,
+        'mean_spl_db': mean_spl_db,
+        'magnitude_db': magnitude_db,
+        'time': time,
+        'duration': duration,
+    }
+    audio_cache[cache_key] = audio_data  # Armazene no cache
+    return audio_data
 
 # Estilo do gráfico
 grafico_config = {
@@ -31,42 +78,6 @@ grafico_config = {
     'font_color': '#d3d3d3',
 }
 
-
-# Função para cálculo das variáveis do áudio
-def calculate_db(audio_data_or_filepath, sr, calibration_factor=1.0):
-    if isinstance(audio_data_or_filepath, str):  # Verifica se é um caminho de arquivo
-        y, sr = librosa.load(audio_data_or_filepath)
-    elif isinstance(audio_data_or_filepath, np.ndarray):  # Verifica se são dados de áudio
-        y = audio_data_or_filepath
-    else:
-        raise TypeError("Entrada inválida. Deve ser um caminho de arquivo (str) ou dados de áudio (np.ndarray).")
-
-    n_fft = 2048
-    hop_length = n_fft // 4
-    stft = librosa.stft(y, n_fft=n_fft, hop_length=hop_length)
-
-    power = np.abs(stft)**2
-    ref_power = np.mean(power)
-    relative_spl_db = 10 * np.log10(power / ref_power) + calibration_factor
-
-    freq = librosa.fft_frequencies(sr=sr, n_fft=n_fft)
-    magnitude_db = librosa.amplitude_to_db(np.abs(stft), ref=np.max)
-    time = librosa.times_like(magnitude_db, sr=sr, hop_length=hop_length)
-    mean_spl_db = np.mean(relative_spl_db, axis=1)
-
-    return freq, mean_spl_db, stft, magnitude_db, time, sr
-
-
-# Função para carregamento dos áudios
-def load_audio(filepath):
-    try:
-        audio, sr = librosa.load(filepath)
-        return audio, sr
-    except Exception as e:
-        print(f'Error loading audio: {e}')
-        return None, None
-
-
 # Função para plotar gráfico do Espectrograma
 def create_spectrogram_figure(magnitude_db, freq, time):
     fig_spectrogram = px.imshow(magnitude_db,
@@ -75,10 +86,9 @@ def create_spectrogram_figure(magnitude_db, freq, time):
                                 aspect='auto', origin='lower',
                                 labels=dict(x="Tempo (s)", y="Frequência (Hz)", color="Magnitude (dB)"),
                                 )
-    fig_spectrogram.update_layout(grafico_config,
-                                  xaxis_title='Tempo (s)', yaxis_title='Frequências (Hz)')
+    fig_spectrogram.update_layout(grafico_config)
     fig_spectrogram.update_yaxes(zeroline=False, type='log',
-                                 tickvals=freq_ticks, ticktext=freq_ticks)
+                                 tickvals=FREQ_TICKS, ticktext=FREQ_TICKS)
 
     return fig_spectrogram
 
@@ -89,11 +99,10 @@ def create_spl_figure(freq, mean_spl_db, name, color):
                       labels={'x': 'Frequência (Hz)', 'y': 'Magnitude (dB Relative)'})
     fig_spl.add_trace(go.Scatter(x=freq, y=mean_spl_db, mode='lines', name=name, line=dict(color=color)))
     fig_spl.update_xaxes(range=[np.log10(20), np.log10(20000)],
-                         tickvals=freq_ticks, ticktext=freq_ticks,
+                         tickvals=FREQ_TICKS, ticktext=FREQ_TICKS,
                          gridcolor='rgba(255, 255, 255, 0.04)')
     fig_spl.update_yaxes(gridcolor='rgba(255, 255, 255, 0.04)', zeroline=False)
     fig_spl.update_layout(grafico_config,
-                          xaxis_title='Frequências (Hz)', yaxis_title='Magnitude (dB relativo)',
                           legend=dict(yanchor="top", y=0.99, xanchor="right", x=0.99))
     return fig_spl
 
@@ -123,46 +132,42 @@ def update_graphs(selected_file, ir_contents, ir_filename):
     audio, sr = None, None
     fig_spectrogram = go.Figure()
     fig_spl = px.line()
-    fig_combined = go.Figure()
     audio_src, ir_audio_src = None, None
 
     audioinfo_children = html.Div([html.P('No audio selected')], className='looper-div')
     irinfo_children = html.Div([html.P('No IR uploaded')], className='irloader-div')
 
     if selected_file:
-        filepath = os.path.join(AUDIO_DIR, selected_file)
-        audio, sr = load_audio(filepath)
+        filepath = AUDIO_DIR / selected_file
+        audio_data = process_audio(filepath) # Use o cache
 
-        if audio is not None:
-
-            duration = librosa.get_duration(y=audio, sr=sr)
-
+        if audio_data:
             audioinfo_children = html.Div([
-                html.P(f'Sample Rate: {sr} Hz'),
-                html.P(f'Duração: {duration:.2f} sec')
+                html.P(f'Sample Rate: {audio_data["sr"]} Hz'),
+                html.P(f'Duração: {audio_data["duration"]:.2f} sec')
             ], className='looper-div')
 
-            audio_src = audio_to_base64(audio, sr)
+            audio_src = audio_to_base64(audio_data['y'], audio_data['sr'])
 
-            freq, mean_spl_db, stft, magnitude_db, time, sr = calculate_db(filepath, sr)
-            fig_spl = create_spl_figure(freq, mean_spl_db, 'SPL Original', '#FF5C00')
-            fig_spectrogram = create_spectrogram_figure(magnitude_db, freq, time)
+            fig_spl = create_spl_figure(audio_data['freq'], audio_data['mean_spl_db'], 'SPL Original', DEFAULT_COLOR)
+            fig_spectrogram = create_spectrogram_figure(audio_data['magnitude_db'], audio_data['freq'], audio_data['time'])
 
-    if ir_contents and audio is not None:  # Process IR only if audio is loaded
+    if ir_contents and audio_data:  # Process IR only if audio is loaded
         content_type, content_string = ir_contents.split(',')
         decoded = base64.b64decode(content_string)
 
         try:
             ir, sr_ir = librosa.load(io.BytesIO(decoded))
-            if sr != sr_ir:
-                ir = librosa.resample(ir, orig_sr=sr_ir, target_sr=sr)
-                print(f"Nova taxa de amostragem do IR: {sr}")
 
-            audio_ir = convolve(audio, ir, mode='same')
+            if audio_data['sr'] != sr_ir:
+                ir = librosa.resample(ir, orig_sr=sr_ir, target_sr=audio_data['sr'])
+
+
+            audio_ir = convolve(audio_data['y'], ir, mode='same') # Use audio_data['y']
             audio_ir /= np.abs(audio_ir).max()
             audio /= np.abs(audio).max()
 
-            freq_ir, mean_spl_db_ir, stft_ir, magnitude_db_ir, time_ir, sr_ir = calculate_db(audio_ir, sr)
+            freq_ir, mean_spl_db_ir, stft_ir, magnitude_db_ir, time_ir, sr_ir = process_audio(audio_ir, sr)
             fig_spl.add_trace(go.Scatter(x=freq_ir, y=mean_spl_db_ir,
                                          mode='lines', name='SPL com IR', line=dict(color='#FFDF00')))
 
@@ -230,18 +235,6 @@ grafico_spl = dcc.Graph(id='spl-graph', ),
 
 grafico_spectrogram = dcc.Graph(id='spectrogram')
 
-span = html.Div(
-            [
-                html.Br(),
-                html.Span('Criado e desenvolvido por '),
-                html.A(
-                    'Eduardo Filho',
-                    href='https://github.com/edborgesfh',
-                    target='_blank',
-                ),
-            ], className='text-center'
-)
-
 # Layout dos pedais
 pedal_looper = dbc.Card([
     dbc.CardBody([
@@ -257,9 +250,9 @@ pedal_looper = dbc.Card([
                     dbc.Col(audio_player),
                 ]),
             ]),
-        ]),
+        ], className='loop-cardbody'),
     ]),
-], className='loop-cardbody'),
+]),
 
 pedal_irloader = dbc.Card([
     dbc.CardBody([
@@ -275,9 +268,9 @@ pedal_irloader = dbc.Card([
                     dbc.Col(ir_audio_player),
                 ]),
             ]),
-        ]),
+        ], className='irloader-cardbody'),
     ]),
-], className='irloader-cardbody'),
+]),
 
 
 # Linhas
@@ -286,8 +279,8 @@ linha_titulo = dbc.Row([
 ])
 
 linha_pedais = dbc.Row([
-    dbc.Col(pedal_looper, lg=4, md=6, sm=12),
-    dbc.Col(pedal_irloader, lg=4, md=6, sm=12)
+    dbc.Col(pedal_looper, width=4),
+    dbc.Col(pedal_irloader, width=4)
 ], justify='center')
 
 linha_graficos = html.Div(id='graphs-container', children=[
@@ -308,16 +301,11 @@ linha_graficos = html.Div(id='graphs-container', children=[
     ]),
 ], style={'display': 'none'})
 
-linha_span = dbc.Row([
-    dbc.Col(span),
-])
-
 app.layout = dbc.Container([
     linha_titulo,
     linha_pedais,
-    linha_graficos,
-    linha_span,
-], fluid=True)
+    linha_graficos
+])
 
 if __name__ == '__main__':
     app.run_server(debug=True, port=8067)
